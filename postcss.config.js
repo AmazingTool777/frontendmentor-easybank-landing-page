b336f747014c995a0e5c8c6592b76177058b80be
Tolojanahary <tolotor@gmail.com>
2024-12-30 13:56:19 +0300

    Mute PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,31 @@
-import { purgeCSSPlugin } from "@fullhuman/postcss-purgecss";
-import postCSSSortMediaQueries from "postcss-sort-media-queries";
-import autoprefixer from "autoprefixer";
+// import { purgeCSSPlugin } from "@fullhuman/postcss-purgecss";
+// import postCSSSortMediaQueries from "postcss-sort-media-queries";
+// import autoprefixer from "autoprefixer";
 
-export default {
-  plugins: [
-    purgeCSSPlugin({
-      content: ["./**/*.html"],
-      extractors: [
-        {
-          extractor: (content) => {
-            return content.match(/[A-z0-9-:\/]+/g);
-          },
-          extensions: ["html"],
-        },
-      ],
-      safelist: [
-        "shadowed",
-        "navbar-menu-overlay",
-        "enter",
-        "enter-from",
-        "enter-to",
-        "leave",
-        "leave-from",
-        "leave-to",
-      ],
-    }),
-    postCSSSortMediaQueries(),
-    autoprefixer(),
-  ],
-};
+// export default {
+//   plugins: [
+//     purgeCSSPlugin({
+//       content: ["./**/*.html"],
+//       extractors: [
+//         {
+//           extractor: (content) => {
+//             return content.match(/[A-z0-9-:\/]+/g);
+//           },
+//           extensions: ["html"],
+//         },
+//       ],
+//       safelist: [
+//         "shadowed",
+//         "navbar-menu-overlay",
+//         "enter",
+//         "enter-from",
+//         "enter-to",
+//         "leave",
+//         "leave-from",
+//         "leave-to",
+//       ],
+//     }),
+//     postCSSSortMediaQueries(),
+//     autoprefixer(),
+//   ],
+// };
